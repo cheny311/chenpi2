9326d7e5821a333e8fc68c63736b7be250dc123c
cheny311 <112643063+cheny311@users.noreply.github.com>
2023-03-29 15:47:42 -0400

    face

---
+++ project/stiches/stiches.js
@@ -0,0 +1,50 @@
+//defining a variable to store the number of clicks
+//reference: samek
+var clicks = 0;
+let pg;
+function setup() {
+ createCanvas(windowHeight,windowWidth);
+ pg = createGraphics(100, 100);
+  //get the reset button element from the HTML document
+  var resetButton = select('#reset-button');
+  
+  //add an event listener to the button element
+  resetButton.mousePressed(resetClicks);
+}
+
+function draw() {
+  //background(100);
+ // pg.background(100,100,100,0);
+
+  //text formatting
+
+  noStroke();
+  textFont('Georgia');
+  textStyle(BOLD);
+  textSize(80);
+  textAlign(CENTER, CENTER);
+ 
+//   fill(255,0,255);
+//   rectMode(CENTER);
+//   rect(width / 2, height / 2, textWidth(clicks.toString()) + 20, textSize() + 20,20);
+ 
+  //displaying number of clicks
+  fill(25);
+  text(clicks, width/2, height/2);
+
+
+  
+}
+
+//function is called when user clicks
+function mousePressed() {
+    clear();
+  //add 1 to variable clicks
+  clicks ++;
+ 
+}
+
+//function to reset the clicks variable to zero
+function resetClicks() {
+  clicks = 0;
+}
