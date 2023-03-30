a2277c68062b88207f5d98a5d0d6d6f7f9f85cfd
cheny311 <112643063+cheny311@users.noreply.github.com>
2023-03-29 23:26:33 -0400

    st

--- project/stiches/stiches.js
+++ project/stiches/stiches.js
@@ -2,9 +2,15 @@
 //reference: samek
 var clicks = 0;
 let pg;
+let img;
+
+function preload() {
+  img = loadImage('yarn.png');
+}
+
 function setup() {
  createCanvas(windowHeight,windowWidth);
- pg = createGraphics(100, 100);
+ 
   //get the reset button element from the HTML document
   var resetButton = select('#reset-button');
   
@@ -13,36 +19,34 @@ function setup() {
 }
 
 function draw() {
-  //background(100);
- // pg.background(100,100,100,0);
-
-  //text formatting
-
   noStroke();
   textFont('Georgia');
   textStyle(BOLD);
   textSize(80);
   textAlign(CENTER, CENTER);
- 
-//   fill(255,0,255);
-//   rectMode(CENTER);
-//   rect(width / 2, height / 2, textWidth(clicks.toString()) + 20, textSize() + 20,20);
+  
+  fill(255,0,255);
+  rectMode(CENTER);
+  rect(width / 2, height / 2, textWidth(clicks.toString()) + 20, textSize() + 20,20);
  
   //displaying number of clicks
   fill(25);
   text(clicks, width/2, height/2);
-
-
+  imageMode(CENTER);
+  image(img, clicks+130, height/2);
+  
   
 }
 
 //function is called when user clicks
 function mousePressed() {
+  
+    // add 1 to variable clicks
+    clicks ++;
     clear();
-  //add 1 to variable clicks
-  clicks ++;
+  }
+
  
-}
 
 //function to reset the clicks variable to zero
 function resetClicks() {
